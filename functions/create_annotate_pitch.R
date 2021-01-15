ed4ab20ec2e1fc346beab11d5b6b767cee7c6eb9
Harry Williams <hgw1@hotmail.co.uk>
2021-01-15 19:59:22 +0000

    fixed pitch scaling

--- functions/create_annotate_pitch.R
+++ functions/create_annotate_pitch.R
@@ -40,43 +40,43 @@ create_annotate_pitch <- function(grass_colour = "#ffffff",
   xmin <- 0 # minimum length
   xmax <- pitch_length + (in_goal * 2) # maximum length
 
-  glxst <- (in_goal / 100) * pitch_length
+  glxst <- in_goal 
   glyst <- (32.5 / 70) * pitch_width
-  glxed <- ((in_goal + 100) / 100) * pitch_length
+  glxed <- in_goal + (100 / 100) * pitch_length
   glyed <- (37.5 / 70) * pitch_width
 
   ## initiate the plot, set some boundaries to the plot
   pitch <- ggplot() +
     xlim(c(xmin - 5, xmax + 5)) +
     ylim(c(ymin - 5, ymax + 5)) +
-    theme_blankPitch() +
+    #theme_blankPitch() +
     # add lines
     # Pitch Outline
     annotate("rect", xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, fill = grass_colour, colour = line_colour) +
     
     # Try line 1
     annotate("segment",
-             x = (in_goal / 100 * pitch_length),
-             y = (0 / 70 * pitch_width),
-             xend = (in_goal / 100 * pitch_length),
-             yend = (70 / 70 * pitch_width),
+             x = in_goal ,
+             y = 0 ,
+             xend = in_goal,
+             yend =  pitch_width,
              colour = line_colour,
              size = 0.5
     ) +
     # 22 1
     annotate("segment",
-             x = ((in_goal + 22) / 100 * pitch_length),
+             x = in_goal + (22 / 100 * pitch_length),
              y = (0 / 70 * pitch_width),
-             xend = ((in_goal + 22)  / 100 * pitch_length),
+             xend = in_goal + (22  / 100 * pitch_length),
              yend = (70 / 70 * pitch_width),
              colour = line_colour,
              size = 0.5
     ) +
     # T10m 1
     annotate("segment",
-             x = ((in_goal + 40) / 100 * pitch_length),
+             x = in_goal + (40 / 100 * pitch_length),
              y = (0 / 70 * pitch_width),
-             xend = ((in_goal + 40) / 100 * pitch_length),
+             xend = in_goal + (40 / 100 * pitch_length),
              yend = (70 / 70 * pitch_width),
              colour = line_colour,
              linetype = "dashed",
@@ -84,18 +84,18 @@ create_annotate_pitch <- function(grass_colour = "#ffffff",
     ) +
     # halfway
     annotate("segment",
-             x = ((in_goal + 50) / 100 * pitch_length),
+             x = in_goal + (50/ 100 * pitch_length),
              y = (0 / 70 * pitch_width),
-             xend = ((in_goal + 50)/ 100 * pitch_length),
+             xend = in_goal + (50/ 100 * pitch_length),
              yend = (70 / 70 * pitch_width),
              colour = line_colour,
              size = 0.5
     ) +
     # T10m 2
     annotate("segment",
-             x = ((in_goal + 60)/ 100 * pitch_length),
+             x = in_goal + (60/ 100 * pitch_length),
              y = (0 / 70 * pitch_width),
-             xend = ((in_goal + 60) / 100 * pitch_length),
+             xend = in_goal + (60 / 100 * pitch_length),
              yend = (70 / 70 * pitch_width),
              colour = line_colour,
              linetype = "dashed",
@@ -103,36 +103,36 @@ create_annotate_pitch <- function(grass_colour = "#ffffff",
     ) +
     # 22 2
     annotate("segment",
-             x = ((in_goal + 78) / 100 * pitch_length),
+             x = in_goal + (78/ 100 * pitch_length),
              y = (0 / 70 * pitch_width),
-             xend = ((in_goal + 78)/ 100 * pitch_length),
+             xend = in_goal + (78/ 100 * pitch_length),
              yend = (70 / 70 * pitch_width),
              colour = line_colour,
              size = 0.5
     ) +
     # try line 2
     annotate("segment",
-             x = ((in_goal + 100) / 100 * pitch_length),
+             x = in_goal + (100 / 100 * pitch_length),
              y = (0 / 70 * pitch_width),
-             xend = ((in_goal + 100)/ 100 * pitch_length),
+             xend = in_goal + (100/ 100 * pitch_length),
              yend = (70 / 70 * pitch_width),
              colour = line_colour,
              size = 0.5
     ) +
     # 5m channel
     annotate("segment",
-             x = ((in_goal)/ 100 * pitch_length),
+             x = in_goal,
              y = (5 / 70 * pitch_width),
-             xend = ((in_goal + 100) / 100 * pitch_length),
+             xend = in_goal + (100 / 100 * pitch_length),
              yend = (5 / 70 * pitch_width),
              colour = line_colour,
              linetype = "dashed",
              size = 0.5
     ) +
     annotate("segment",
-             x = ((in_goal + 5) / 100 * pitch_length),
+             x = in_goal + (5 / 100 * pitch_length),
              y = (2 / 70 * pitch_width),
-             xend = ((in_goal + 5)/ 100 * pitch_length),
+             xend = in_goal + (5/ 100 * pitch_length),
              yend = (8 / 70 * pitch_width),
              colour = line_colour,
              size = 0.5
@@ -140,131 +140,131 @@ create_annotate_pitch <- function(grass_colour = "#ffffff",
     
     # 5m channel
     annotate("segment",
-             x = ((in_goal)/ 100 * pitch_length),
+             x = in_goal,
              y = (65 / 70 * pitch_width),
-             xend = ((in_goal + 100)/ 100 * pitch_length),
+             xend = in_goal + (100/ 100 * pitch_length),
              yend = (65 / 70 * pitch_width),
              colour = line_colour,
              linetype = "dashed",
              size = 0.5
     ) +
     annotate("segment",
-             x = ((in_goal + 95)/ 100 * pitch_length),
+             x = in_goal + (95/ 100 * pitch_length),
              y = (2 / 70 * pitch_width),
-             xend = ((in_goal + 95)/ 100 * pitch_length),
+             xend = in_goal + (95/ 100 * pitch_length),
              yend = (8 / 70 * pitch_width),
              colour = line_colour,
              size = 0.5
     ) +
     # 5m lines
     annotate("segment",
-             x = ((in_goal + 5) / 100 * pitch_length),
+             x = in_goal + (5 / 100 * pitch_length),
              y = (12 / 70 * pitch_width),
-             xend = ((in_goal + 5)/ 100 * pitch_length),
+             xend = in_goal + (5 / 100 * pitch_length),
              yend = (18 / 70 * pitch_width),
              colour = line_colour,
              size = 0.5
     ) +
     annotate("segment",
-             x = ((in_goal + 95) / 100 * pitch_length),
+             x = in_goal + (95 / 100 * pitch_length),
              y = (12 / 70 * pitch_width),
-             xend = ((in_goal + 95)/ 100 * pitch_length),
+             xend = in_goal + (95 / 100 * pitch_length),
              yend = (18 / 70 * pitch_width),
              colour = line_colour,
              size = 0.5
     ) +
     # 15m line
     annotate("segment",
-             x = ((in_goal) / 100 * pitch_length),
+             x = in_goal,
              y = (15 / 70 * pitch_width),
-             xend = ((in_goal + 5) / 100 * pitch_length),
+             xend = in_goal + (5 / 100 * pitch_length),
              yend = (15 / 70 * pitch_width),
              colour = line_colour,
              size = 0.5
     ) +
     annotate("segment",
-             x = ((in_goal + 19)/ 100 * pitch_length),
+             x = in_goal + (19/ 100 * pitch_length),
              y = (15 / 70 * pitch_width),
-             xend = ((in_goal + 25)/ 100 * pitch_length),
+             xend = in_goal + (25/ 100 * pitch_length),
              yend = (15 / 70 * pitch_width),
              colour = line_colour,
              size = 0.5
     ) +
     annotate("segment",
-             x = ((in_goal + 37) / 100 * pitch_length),
+             x = in_goal + (37 / 100 * pitch_length),
              y = (15 / 70 * pitch_width),
-             xend = ((in_goal + 43) / 100 * pitch_length),
+             xend = in_goal + (43 / 100 * pitch_length),
              yend = (15 / 70 * pitch_width),
              colour = line_colour,
              size = 0.5
     ) +
     annotate("segment",
-             x = ((in_goal + 47) / 100 * pitch_length),
+             x = in_goal + (47 / 100 * pitch_length),
              y = (15 / 70 * pitch_width),
-             xend = ((in_goal + 53) / 100 * pitch_length),
+             xend = in_goal + (53 / 100 * pitch_length),
              yend = (15 / 70 * pitch_width),
              colour = line_colour,
              size = 0.5
     ) +
     annotate("segment",
-             x = ((in_goal + 57) / 100 * pitch_length),
+             x = in_goal + (57 / 100 * pitch_length),
              y = (15 / 70 * pitch_width),
-             xend = ((in_goal + 63) / 100 * pitch_length),
+             xend = in_goal + (63 / 100 * pitch_length),
              yend = (15 / 70 * pitch_width),
              colour = line_colour,
              size = 0.5
     ) +
     annotate("segment",
-             x = ((in_goal + 75) / 100 * pitch_length),
+             x = in_goal + (75 / 100 * pitch_length),
              y = (15 / 70 * pitch_width),
-             xend = ((in_goal + 81) / 100 * pitch_length),
+             xend = in_goal + (81 / 100 * pitch_length),
              yend = (15 / 70 * pitch_width),
              colour = line_colour,
              size = 0.5
     ) +
     annotate("segment",
-             x = ((in_goal + 100) / 100 * pitch_length),
+             x = in_goal + (100 / 100 * pitch_length),
              y = (15 / 70 * pitch_width),
-             xend = ((in_goal + 95) / 100 * pitch_length),
+             xend = in_goal + (95 / 100 * pitch_length),
              yend = (15 / 70 * pitch_width),
              colour = line_colour,
              size = 0.5
     ) +
     annotate("segment",
-             x = ((in_goal + 5) / 100 * pitch_length),
+             x = in_goal + (5 / 100 * pitch_length),
              y = (62 / 70 * pitch_width),
-             xend = ((in_goal + 5)/ 100 * pitch_length),
+             xend = in_goal +( 5 / 100 * pitch_length),
              yend = (68 / 70 * pitch_width),
              colour = line_colour,
              size = 0.5
     ) +
     annotate("segment",
-             x = ((in_goal + 95) / 100 * pitch_length),
+             x = in_goal + (95 / 100 * pitch_length),
              y = (62 / 70 * pitch_width),
-             xend = ((in_goal + 95)/ 100 * pitch_length),
+             xend = in_goal + (95 / 100 * pitch_length),
              yend = (68 / 70 * pitch_width),
              colour = line_colour,
              size = 0.5
     ) +
     # 15m channel
     annotate("segment",
-             x = ((in_goal + 5) / 100 * pitch_length),
+             x = in_goal + (5 / 100 * pitch_length),
              y = (52 / 70 * pitch_width),
-             xend = ((in_goal + 5)/ 100 * pitch_length),
+             xend = in_goal + (5 / 100 * pitch_length),
              yend = (58 / 70 * pitch_width),
              colour = line_colour,
              size = 0.5
     ) +
     annotate("segment",
-             x = ((in_goal + 95) / 100 * pitch_length),
+             x = in_goal + (95 / 100 * pitch_length),
              y = (52 / 70 * pitch_width),
-             xend = ((in_goal + 95) / 100 * pitch_length),
+             xend = in_goal + (95 / 100 * pitch_length),
              yend = (58 / 70 * pitch_width),
              colour = line_colour,
              size = 0.5
     ) +
     annotate("segment",
-             x = ((in_goal)/ 100 * pitch_length),
+             x = in_goal,
              y = (55 / 70 * pitch_width),
              xend = ((in_goal + 5)/ 100 * pitch_length),
              yend = (55 / 70 * pitch_width),
@@ -272,49 +272,49 @@ create_annotate_pitch <- function(grass_colour = "#ffffff",
              size = 0.5
     ) +
     annotate("segment",
-             x = ((in_goal + 19) / 100 * pitch_length),
+             x = in_goal + (19 / 100 * pitch_length),
              y = (55 / 70 * pitch_width),
-             xend = ((in_goal + 25) / 100 * pitch_length),
+             xend = in_goal + (25 / 100 * pitch_length),
              yend = (55 / 70 * pitch_width),
              colour = line_colour,
              size = 0.5
     ) +
     annotate("segment",
-             x = ((in_goal + 37) / 100 * pitch_length),
+             x = in_goal + (37  / 100 * pitch_length),
              y = (55 / 70 * pitch_width),
-             xend = ((in_goal + 43) / 100 * pitch_length),
+             xend = in_goal + (43 / 100 * pitch_length),
              yend = (55 / 70 * pitch_width),
              colour = line_colour,
              size = 0.5
     ) +
     annotate("segment",
-             x = ((in_goal + 47) / 100 * pitch_length),
+             x = in_goal +( 47 / 100 * pitch_length),
              y = (55 / 70 * pitch_width),
-             xend = ((in_goal + 53) / 100 * pitch_length),
+             xend = in_goal + (53 / 100 * pitch_length),
              yend = (55 / 70 * pitch_width),
              colour = line_colour,
              size = 0.5)+
     
     annotate("segment",
-             x = ((in_goal + 57)/ 100 * pitch_length),
+             x = in_goal + (57/ 100 * pitch_length),
              y = (55 / 70 * pitch_width),
-             xend = ((in_goal + 63)/ 100 * pitch_length),
+             xend = in_goal + (63/ 100 * pitch_length),
              yend = (55 / 70 * pitch_width),
              colour = line_colour,
              size = 0.5
     ) +
     annotate("segment",
-             x = ((in_goal + 75) / 100 * pitch_length),
+             x = in_goal + (75 / 100 * pitch_length),
              y = (55 / 70 * pitch_width),
-             xend = ((in_goal + 81)/ 100 * pitch_length),
+             xend = in_goal + (81/ 100 * pitch_length),
              yend = (55 / 70 * pitch_width),
              colour = line_colour,
              size = 0.5
     ) +
     annotate("segment",
-             x = ((in_goal + 100)/ 100 * pitch_length),
+             x = in_goal + (100/ 100 * pitch_length),
              y = (55 / 70 * pitch_width),
-             xend = ((in_goal + 95) / 100 * pitch_length),
+             xend = in_goal + (95  / 100 * pitch_length),
              yend = (55 / 70 * pitch_width),
              colour = line_colour,
              size = 0.5
--- functions/create_rugby_pitch_lines.R
+++ functions/create_rugby_pitch_lines.R
@@ -11,38 +11,36 @@ create_rugby_pitch_lines <- function(plot, grass_colour = "#ffffff",
   xmin <- 0 # minimum length
   xmax <- pitch_length + (in_goal * 2) # maximum length
   
-  glxst <- (in_goal / 100) * pitch_length
+  glxst <- in_goal 
   glyst <- (32.5 / 70) * pitch_width
-  glxed <- ((in_goal + 100) / 100) * pitch_length
+  glxed <- in_goal + (100 / 100) * pitch_length
   glyed <- (37.5 / 70) * pitch_width
-  
   
   # Pitch Outline
 p <-  plot + 
-  
   # Try line 1
   annotate("segment",
-           x = (in_goal / 100 * pitch_length),
-           y = (0 / 70 * pitch_width),
-           xend = (in_goal / 100 * pitch_length),
-           yend = (70 / 70 * pitch_width),
+           x = in_goal ,
+           y = 0 ,
+           xend = in_goal,
+           yend =  pitch_width,
            colour = line_colour,
            size = 0.5
   ) +
   # 22 1
   annotate("segment",
-           x = ((in_goal + 22) / 100 * pitch_length),
+           x = in_goal + (22 / 100 * pitch_length),
            y = (0 / 70 * pitch_width),
-           xend = ((in_goal + 22)  / 100 * pitch_length),
+           xend = in_goal + (22  / 100 * pitch_length),
            yend = (70 / 70 * pitch_width),
            colour = line_colour,
            size = 0.5
   ) +
   # T10m 1
   annotate("segment",
-           x = ((in_goal + 40) / 100 * pitch_length),
+           x = in_goal + (40 / 100 * pitch_length),
            y = (0 / 70 * pitch_width),
-           xend = ((in_goal + 40) / 100 * pitch_length),
+           xend = in_goal + (40 / 100 * pitch_length),
            yend = (70 / 70 * pitch_width),
            colour = line_colour,
            linetype = "dashed",
@@ -50,18 +48,18 @@ p <-  plot +
   ) +
   # halfway
   annotate("segment",
-           x = ((in_goal + 50) / 100 * pitch_length),
+           x = in_goal + (50/ 100 * pitch_length),
            y = (0 / 70 * pitch_width),
-           xend = ((in_goal + 50)/ 100 * pitch_length),
+           xend = in_goal + (50/ 100 * pitch_length),
            yend = (70 / 70 * pitch_width),
            colour = line_colour,
            size = 0.5
   ) +
   # T10m 2
   annotate("segment",
-           x = ((in_goal + 60)/ 100 * pitch_length),
+           x = in_goal + (60/ 100 * pitch_length),
            y = (0 / 70 * pitch_width),
-           xend = ((in_goal + 60) / 100 * pitch_length),
+           xend = in_goal + (60 / 100 * pitch_length),
            yend = (70 / 70 * pitch_width),
            colour = line_colour,
            linetype = "dashed",
@@ -69,36 +67,36 @@ p <-  plot +
   ) +
   # 22 2
   annotate("segment",
-           x = ((in_goal + 78) / 100 * pitch_length),
+           x = in_goal + (78/ 100 * pitch_length),
            y = (0 / 70 * pitch_width),
-           xend = ((in_goal + 78)/ 100 * pitch_length),
+           xend = in_goal + (78/ 100 * pitch_length),
            yend = (70 / 70 * pitch_width),
            colour = line_colour,
            size = 0.5
   ) +
   # try line 2
   annotate("segment",
-           x = ((in_goal + 100) / 100 * pitch_length),
+           x = in_goal + (100 / 100 * pitch_length),
            y = (0 / 70 * pitch_width),
-           xend = ((in_goal + 100)/ 100 * pitch_length),
+           xend = in_goal + (100/ 100 * pitch_length),
            yend = (70 / 70 * pitch_width),
            colour = line_colour,
            size = 0.5
   ) +
   # 5m channel
   annotate("segment",
-           x = ((in_goal)/ 100 * pitch_length),
+           x = in_goal,
            y = (5 / 70 * pitch_width),
-           xend = ((in_goal + 100) / 100 * pitch_length),
+           xend = in_goal + (100 / 100 * pitch_length),
            yend = (5 / 70 * pitch_width),
            colour = line_colour,
            linetype = "dashed",
            size = 0.5
   ) +
   annotate("segment",
-           x = ((in_goal + 5) / 100 * pitch_length),
+           x = in_goal + (5 / 100 * pitch_length),
            y = (2 / 70 * pitch_width),
-           xend = ((in_goal + 5)/ 100 * pitch_length),
+           xend = in_goal + (5/ 100 * pitch_length),
            yend = (8 / 70 * pitch_width),
            colour = line_colour,
            size = 0.5
@@ -106,131 +104,131 @@ p <-  plot +
   
   # 5m channel
   annotate("segment",
-           x = ((in_goal)/ 100 * pitch_length),
+           x = in_goal,
            y = (65 / 70 * pitch_width),
-           xend = ((in_goal + 100)/ 100 * pitch_length),
+           xend = in_goal + (100/ 100 * pitch_length),
            yend = (65 / 70 * pitch_width),
            colour = line_colour,
            linetype = "dashed",
            size = 0.5
   ) +
   annotate("segment",
-           x = ((in_goal + 95)/ 100 * pitch_length),
+           x = in_goal + (95/ 100 * pitch_length),
            y = (2 / 70 * pitch_width),
-           xend = ((in_goal + 95)/ 100 * pitch_length),
+           xend = in_goal + (95/ 100 * pitch_length),
            yend = (8 / 70 * pitch_width),
            colour = line_colour,
            size = 0.5
   ) +
   # 5m lines
   annotate("segment",
-           x = ((in_goal + 5) / 100 * pitch_length),
+           x = in_goal + (5 / 100 * pitch_length),
            y = (12 / 70 * pitch_width),
-           xend = ((in_goal + 5)/ 100 * pitch_length),
+           xend = in_goal + (5 / 100 * pitch_length),
            yend = (18 / 70 * pitch_width),
            colour = line_colour,
            size = 0.5
   ) +
   annotate("segment",
-           x = ((in_goal + 95) / 100 * pitch_length),
+           x = in_goal + (95 / 100 * pitch_length),
            y = (12 / 70 * pitch_width),
-           xend = ((in_goal + 95)/ 100 * pitch_length),
+           xend = in_goal + (95 / 100 * pitch_length),
            yend = (18 / 70 * pitch_width),
            colour = line_colour,
            size = 0.5
   ) +
   # 15m line
   annotate("segment",
-           x = ((in_goal) / 100 * pitch_length),
+           x = in_goal,
            y = (15 / 70 * pitch_width),
-           xend = ((in_goal + 5) / 100 * pitch_length),
+           xend = in_goal + (5 / 100 * pitch_length),
            yend = (15 / 70 * pitch_width),
            colour = line_colour,
            size = 0.5
   ) +
   annotate("segment",
-           x = ((in_goal + 19)/ 100 * pitch_length),
+           x = in_goal + (19/ 100 * pitch_length),
            y = (15 / 70 * pitch_width),
-           xend = ((in_goal + 25)/ 100 * pitch_length),
+           xend = in_goal + (25/ 100 * pitch_length),
            yend = (15 / 70 * pitch_width),
            colour = line_colour,
            size = 0.5
   ) +
   annotate("segment",
-           x = ((in_goal + 37) / 100 * pitch_length),
+           x = in_goal + (37 / 100 * pitch_length),
            y = (15 / 70 * pitch_width),
-           xend = ((in_goal + 43) / 100 * pitch_length),
+           xend = in_goal + (43 / 100 * pitch_length),
            yend = (15 / 70 * pitch_width),
            colour = line_colour,
            size = 0.5
   ) +
   annotate("segment",
-           x = ((in_goal + 47) / 100 * pitch_length),
+           x = in_goal + (47 / 100 * pitch_length),
            y = (15 / 70 * pitch_width),
-           xend = ((in_goal + 53) / 100 * pitch_length),
+           xend = in_goal + (53 / 100 * pitch_length),
            yend = (15 / 70 * pitch_width),
            colour = line_colour,
            size = 0.5
   ) +
   annotate("segment",
-           x = ((in_goal + 57) / 100 * pitch_length),
+           x = in_goal + (57 / 100 * pitch_length),
            y = (15 / 70 * pitch_width),
-           xend = ((in_goal + 63) / 100 * pitch_length),
+           xend = in_goal + (63 / 100 * pitch_length),
            yend = (15 / 70 * pitch_width),
            colour = line_colour,
            size = 0.5
   ) +
   annotate("segment",
-           x = ((in_goal + 75) / 100 * pitch_length),
+           x = in_goal + (75 / 100 * pitch_length),
            y = (15 / 70 * pitch_width),
-           xend = ((in_goal + 81) / 100 * pitch_length),
+           xend = in_goal + (81 / 100 * pitch_length),
            yend = (15 / 70 * pitch_width),
            colour = line_colour,
            size = 0.5
   ) +
   annotate("segment",
-           x = ((in_goal + 100) / 100 * pitch_length),
+           x = in_goal + (100 / 100 * pitch_length),
            y = (15 / 70 * pitch_width),
-           xend = ((in_goal + 95) / 100 * pitch_length),
+           xend = in_goal + (95 / 100 * pitch_length),
            yend = (15 / 70 * pitch_width),
            colour = line_colour,
            size = 0.5
   ) +
   annotate("segment",
-           x = ((in_goal + 5) / 100 * pitch_length),
+           x = in_goal + (5 / 100 * pitch_length),
            y = (62 / 70 * pitch_width),
-           xend = ((in_goal + 5)/ 100 * pitch_length),
+           xend = in_goal +( 5 / 100 * pitch_length),
            yend = (68 / 70 * pitch_width),
            colour = line_colour,
            size = 0.5
   ) +
   annotate("segment",
-           x = ((in_goal + 95) / 100 * pitch_length),
+           x = in_goal + (95 / 100 * pitch_length),
            y = (62 / 70 * pitch_width),
-           xend = ((in_goal + 95)/ 100 * pitch_length),
+           xend = in_goal + (95 / 100 * pitch_length),
            yend = (68 / 70 * pitch_width),
            colour = line_colour,
            size = 0.5
   ) +
   # 15m channel
   annotate("segment",
-           x = ((in_goal + 5) / 100 * pitch_length),
+           x = in_goal + (5 / 100 * pitch_length),
            y = (52 / 70 * pitch_width),
-           xend = ((in_goal + 5)/ 100 * pitch_length),
+           xend = in_goal + (5 / 100 * pitch_length),
            yend = (58 / 70 * pitch_width),
            colour = line_colour,
            size = 0.5
   ) +
   annotate("segment",
-           x = ((in_goal + 95) / 100 * pitch_length),
+           x = in_goal + (95 / 100 * pitch_length),
            y = (52 / 70 * pitch_width),
-           xend = ((in_goal + 95) / 100 * pitch_length),
+           xend = in_goal + (95 / 100 * pitch_length),
            yend = (58 / 70 * pitch_width),
            colour = line_colour,
            size = 0.5
   ) +
   annotate("segment",
-           x = ((in_goal)/ 100 * pitch_length),
+           x = in_goal,
            y = (55 / 70 * pitch_width),
            xend = ((in_goal + 5)/ 100 * pitch_length),
            yend = (55 / 70 * pitch_width),
@@ -238,49 +236,49 @@ p <-  plot +
            size = 0.5
   ) +
   annotate("segment",
-           x = ((in_goal + 19) / 100 * pitch_length),
+           x = in_goal + (19 / 100 * pitch_length),
            y = (55 / 70 * pitch_width),
-           xend = ((in_goal + 25) / 100 * pitch_length),
+           xend = in_goal + (25 / 100 * pitch_length),
            yend = (55 / 70 * pitch_width),
            colour = line_colour,
            size = 0.5
   ) +
   annotate("segment",
-           x = ((in_goal + 37) / 100 * pitch_length),
+           x = in_goal + (37  / 100 * pitch_length),
            y = (55 / 70 * pitch_width),
-           xend = ((in_goal + 43) / 100 * pitch_length),
+           xend = in_goal + (43 / 100 * pitch_length),
            yend = (55 / 70 * pitch_width),
            colour = line_colour,
            size = 0.5
   ) +
   annotate("segment",
-           x = ((in_goal + 47) / 100 * pitch_length),
+           x = in_goal +( 47 / 100 * pitch_length),
            y = (55 / 70 * pitch_width),
-           xend = ((in_goal + 53) / 100 * pitch_length),
+           xend = in_goal + (53 / 100 * pitch_length),
            yend = (55 / 70 * pitch_width),
            colour = line_colour,
            size = 0.5)+
   
   annotate("segment",
-           x = ((in_goal + 57)/ 100 * pitch_length),
+           x = in_goal + (57/ 100 * pitch_length),
            y = (55 / 70 * pitch_width),
-           xend = ((in_goal + 63)/ 100 * pitch_length),
+           xend = in_goal + (63/ 100 * pitch_length),
            yend = (55 / 70 * pitch_width),
            colour = line_colour,
            size = 0.5
   ) +
   annotate("segment",
-           x = ((in_goal + 75) / 100 * pitch_length),
+           x = in_goal + (75 / 100 * pitch_length),
            y = (55 / 70 * pitch_width),
-           xend = ((in_goal + 81)/ 100 * pitch_length),
+           xend = in_goal + (81/ 100 * pitch_length),
            yend = (55 / 70 * pitch_width),
            colour = line_colour,
            size = 0.5
   ) +
   annotate("segment",
-           x = ((in_goal + 100)/ 100 * pitch_length),
+           x = in_goal + (100/ 100 * pitch_length),
            y = (55 / 70 * pitch_width),
-           xend = ((in_goal + 95) / 100 * pitch_length),
+           xend = in_goal + (95  / 100 * pitch_length),
            yend = (55 / 70 * pitch_width),
            colour = line_colour,
            size = 0.5
